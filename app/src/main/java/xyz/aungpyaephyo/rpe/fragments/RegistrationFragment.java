8ab5375280c6878393da07165b2a97930b10159d
Aung Pyae Phyo <aungpyaephyo24@gmail.com>
2016-06-25 22:16:01 +0630

    Initial Commit.

---
+++ app/src/main/java/xyz/aungpyaephyo/rpe/fragments/RegistrationFragment.java
@@ -0,0 +1,89 @@
+package xyz.aungpyaephyo.rpe.fragments;
+
+import android.os.Bundle;
+import android.support.v4.app.DialogFragment;
+import android.support.v4.app.Fragment;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.CompoundButton;
+import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.Toast;
+
+import com.wdullaer.materialdatetimepicker.date.DatePickerDialog;
+
+import java.util.Calendar;
+
+import xyz.aungpyaephyo.rpe.R;
+import xyz.aungpyaephyo.rpe.dialogs.MyDatePickerDialog;
+
+/**
+ * A placeholder fragment containing a simple view.
+ */
+public class RegistrationFragment extends Fragment implements DatePickerDialog.OnDateSetListener {
+
+    private EditText etDateOfBirth;
+
+    public RegistrationFragment() {
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_registration, container, false);
+
+        etDateOfBirth = (EditText) view.findViewById(R.id.et_date_of_birth);
+
+        etDateOfBirth.setOnFocusChangeListener(new View.OnFocusChangeListener() {
+            @Override
+            public void onFocusChange(View v, boolean hasFocus) {
+                if (hasFocus) {
+                    showThirdPartyDatePicker();
+                    //showDatePicker();
+                }
+            }
+        });
+
+        etDateOfBirth.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showThirdPartyDatePicker();
+                //showDatePicker();
+            }
+        });
+
+        RadioButton rbSoftwareEngineer = (RadioButton) view.findViewById(R.id.rb_software_engineer);
+        rbSoftwareEngineer.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                Toast.makeText(getContext(), "rbSoftwareEngineer : "+isChecked, Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        return view;
+    }
+
+    private void showDatePicker(){
+        DialogFragment newFragment = new MyDatePickerDialog();
+        newFragment.show(getActivity().getSupportFragmentManager(), "datePicker");
+    }
+
+    private void showThirdPartyDatePicker(){
+        Calendar now = Calendar.getInstance();
+        DatePickerDialog thirdPartyDatePicker = DatePickerDialog.newInstance(
+                this,
+                now.get(Calendar.YEAR),
+                now.get(Calendar.MONTH),
+                now.get(Calendar.DAY_OF_MONTH)
+        );
+        thirdPartyDatePicker.show(getActivity().getFragmentManager(), "ThirdPartyDatePicker");
+
+    }
+
+    @Override
+    public void onDateSet(DatePickerDialog view, int year, int monthOfYear, int dayOfMonth) {
+        Toast.makeText(getContext(), "Year : " + year + " Month : " + monthOfYear + " Day : " + dayOfMonth, Toast.LENGTH_SHORT).show();
+    }
+}
